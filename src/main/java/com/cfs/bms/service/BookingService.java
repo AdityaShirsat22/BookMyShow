72b41a1d92a3a8ef3e5206d9008fc6aa345dd72a
Aditya <adityashirsat1170@gmail.com>
2025-10-27 00:11:12 +0530

    minute changes in bookingService

--- src/main/java/com/cfs/bms/service/BookingService.java
+++ src/main/java/com/cfs/bms/service/BookingService.java
@@ -77,7 +77,7 @@ public class BookingService {
             seat.setBooking(saveBooking);
         });
         showSeatRepository.saveAll(selectedSeats);
-        return
+        return mapToBookingDto(saveBooking,selectedSeats);
 
 
     }
